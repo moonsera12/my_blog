8151be0f33d1373ce2e81888b8d9159631213190
moonsera12 <your.email@example.com>
2025-06-18 00:24:50 +0900

    Simplify next.config.js and fix Node.js version

--- next.config.js
+++ next.config.js
@@ -1,16 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
   images: {
-    domains: ['images.unsplash.com'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '*',
-      },
-    ],
-  },
+    domains: ['images.unsplash.com']
+  }
 }
 
 module.exports = nextConfig
